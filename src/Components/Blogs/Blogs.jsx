8fbc94aca42f4e8c4999754e84566690a6eb8ec7
Aminur Rahman <dev.aminur@gmail.com>
2023-09-13 11:39:18 +0600

    Blogs designed

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -9,39 +9,46 @@ const Blogs = () => {
       .then((data) => setBlogs(data));
   }, []);
   return (
-    <div>
-      <h1>Blog: {blogs.length}</h1>
+    <div className="w-2/3">
       {blogs.map((blog) => (
-        <div key={blog.id}>
-          <img src={blog.cover} alt="" />
-          <div className="flex">
-            <div className="flex justify-between">
-              <img src={blog["author-img"]} alt="" />
+        <div className="py-4 border-b border-gray-200 mb-5" key={blog.id}>
+          <img className="mb-8" src={blog.cover} alt="" />
+          <div className="flex justify-between items-center">
+            <div className="flex gap-6">
+              <img className="w-14" src={blog["author-img"]} alt="" />
               <div>
-                <h4>{blog.author}</h4>
-                <p>{blog["posted-date"]}</p>
+                <h4 className="text-2xl font-bold text-[#111]">
+                  {blog.author}
+                </h4>
+                <p className="text-gray-400 text-sm font-medium">
+                  {blog["posted-date"]}
+                </p>
               </div>
             </div>
             <div>
-              <p>
+              <p className="text-gray-400 font-semibold">
                 {blog["reading-time"]}{" "}
-                <button>
+                <button className="ml-2 text-gray-500">
                   <FaBookmark />
                 </button>
               </p>
             </div>
           </div>
-          <h2>{blog.title}</h2>
+          <h2 className="text-4xl font-bold text-[#111] mt-5 mb-3">
+            {blog.title}
+          </h2>
           {blog.hashtags.map((hashtag) => (
             <a
               href={hashtag}
-              className="mr-3"
+              className="mr-2 text-sm text-gray-400 font-semibold"
               key={blog.hashtags.indexOf(hashtag)}
             >
               {hashtag}
             </a>
           ))}
-          <button className="block">Mark as read</button>
+          <button className="block mt-2 font-semibold text-lg text-[#6047EC] underline">
+            Mark as read
+          </button>
         </div>
       ))}
     </div>
